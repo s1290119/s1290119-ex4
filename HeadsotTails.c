486dc17387ef501cabbcb6f457bea97fe64d3828
s1290119 <109216480+s1290119@users.noreply.github.com>
2022-07-13 21:22:39 +0900

    User name added

--- HeadsotTails.c
+++ HeadsotTails.c
@@ -1,11 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
 int main(){
     srand((unsigned int)time(NULL));
     int Heads = 0, Tails = 0, round = 3;
+    char username[256];
 
+    printf("Who are you?\n> ");
+    if (fgets(username, 256, stdin) == NULL)
+    {
+        fprintf(stderr, "error");
+        exit(1);
+    }
+    if (strchr(username, '\n') != NULL)
+    {
+        username[strlen(username)-1] = '\0';
+    }
+
+    printf("Hello, %s!\n", username);
     printf("Tossing a coin...\n");
     for (int i = 0; i < round; i++)
     {
